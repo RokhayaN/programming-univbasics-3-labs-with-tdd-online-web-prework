c0146bdbccd7a5995eeb438e27355270024adf07
RokhayaN <rokhayamuslima1@gmail.com>
2019-09-30 19:10:25 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,9 @@
-first_number = 8 # Add your variables helper_method
+first_number = 8 # Add your variables here
 second_number = 4
 
 sum = first_number + second_number
-difference = first_number 
+difference = first_number - second_number
+product = 
 
 
 
