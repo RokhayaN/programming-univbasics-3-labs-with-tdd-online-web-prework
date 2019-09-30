89ae733c083a539962e91f717edec518e0f265e6
RokhayaN <rokhayamuslima1@gmail.com>
2019-09-30 19:12:05 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,7 +3,8 @@ second_number = 4
 
 sum = first_number + second_number
 difference = first_number - second_number
-product = 
+product = first_number * second_number
+quotient = first number  second_number
 
 
 
