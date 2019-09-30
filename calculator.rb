d1ce440f14c87a6acf817005a7823560ce7c8a97
RokhayaN <rokhayamuslima1@gmail.com>
2019-09-30 18:44:27 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,7 +3,9 @@ def first_number('./calculator.rb',"1")
   puts 'first_number' 
 end 
 second_number =! 0 
-  puts 'first_number' += 1 
+second_number = first_number =+ 1 
+def second_number
+  puts first_number += 1 
 end 
 
 
